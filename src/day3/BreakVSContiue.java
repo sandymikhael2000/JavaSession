9e204628b0c5883ac536785280c0ca1229e65e98
Sandy Mikhael <sandymikhael2000@gmail.com>
2025-12-01 19:40:33 -0500

    Remove one Printline

--- src/day3/BreakVSContiue.java
+++ src/day3/BreakVSContiue.java
@@ -11,7 +11,6 @@ public class BreakVSContiue {
         System.out.println("------------------------");
         System.out.println("------------------------");
         System.out.println("------------------------");
-        System.out.println("------------------------");
         for (int i=1; i<=10;i++){
             if(i==5){
                 continue;
