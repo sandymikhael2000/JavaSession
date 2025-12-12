b095dfce2c2e40e82a5ed5a985d7ef362afe11ae
Sandy Mikhael <sandymikhael2000@gmail.com>
2025-12-12 10:27:47 -0500

    I removed the comment from the code

--- src/day2/LoopingStatment.java
+++ src/day2/LoopingStatment.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class LoopingStatment {
     public static void main(String[] args) {
 // While Loop
-//int rate =15;
-//int maxHoursPerWeek=40;
-//        Scanner x =new Scanner(System.in);
-//        System.out.println("Enter Your Working Hours");
-//        int WorkedHours=x.nextInt();
-//         while (WorkedHours>maxHoursPerWeek)
-//         {
-//             System.out.println("Please enter num between 1  and 40 ");
-//             WorkedHours=x.nextInt();
-//         }
-//         int grossPay =WorkedHours*rate;
-//        System.out.println(grossPay);
-//        x.close();
+int rate =15;
+int maxHoursPerWeek=40;
+        Scanner x =new Scanner(System.in);
+        System.out.println("Enter Your Working Hours");
+        int WorkedHours=x.nextInt();
+         while (WorkedHours>maxHoursPerWeek)
+         {
+             System.out.println("Please enter num between 1  and 40 ");
+             WorkedHours=x.nextInt();
+         }
+         int grossPay =WorkedHours*rate;
+        System.out.println(grossPay);
+        x.close();
 
 
 
